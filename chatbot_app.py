9d70a4f169cbdcdb70085c9fe66f61dcabbb6ae1
Varada Raja Reddy Peddyreddy <141263045+Varada-Raja-Reddy-Peddyreddy@users.noreply.github.com>
2024-12-21 12:05:13 +0530

    Add files via upload

---
+++ chatbot_app.py
@@ -0,0 +1,86 @@
+import streamlit as st
+from typing import Annotated
+from typing_extensions import TypedDict
+from langgraph.graph import StateGraph
+from langgraph.graph.message import add_messages
+from langchain_groq import ChatGroq
+from secret_api_keys import Groq_API_Key
+
+import os
+os.environ['groq_api_key']=Groq_API_Key
+
+st.set_page_config(page_title="Chatbot",page_icon="🤖")
+st.title("Langgraph Chatbot")
+
+# initialize llm
+llm = ChatGroq(api_key = Groq_API_Key,model_name =   "llama-3.2-90b-vision-preview")
+
+
+class State(TypedDict):
+    messages : Annotated [list,add_messages]
+
+# create Graph_builder for creating StateGraph instance
+
+Graph_builder = StateGraph(State)
+
+#define a chatbot
+def chatbot(State:State):
+    return {"messages":[llm.invoke(State["messages"])]}
+
+# Add chatbot to the graph
+
+Graph_builder.add_node("chatbot",chatbot)
+Graph_builder.set_entry_point("chatbot")
+Graph_builder.set_finish_point("chatbot")
+
+#compiler the graph
+graph = Graph_builder.compile()
+
+#Function to Stream Updates
+def stream_graph_updates(user_input:str):
+    initial_state = {"messages":[("user",user_input)]}
+    responses = []
+    for event in graph.stream(initial_state):
+        for value in event.values():
+            responses.append(value["messages"][-1].content)
+    return responses
+
+# initialize the session state to store conversation history
+if "messages" not in st. session_state:
+    st.session_state['messages']=[]
+
+# sidebar fro userinput
+
+def chatbot_sidebar():
+    st.sidebar.title("Chat With the Assistant")
+
+    # input box for user message on the sidebar
+    user_input = st.sidebar.text_input("Your message",key="input",placeholder="Type Your Message here...")
+
+    # submit button in the sidebar
+    if st.sidebar.button("send"):
+        submit_message(user_input)
+
+# Function to submit the message and Generate the message
+def submit_message(user_input):
+    if user_input:
+        #Append input to conversation History
+        st.session_state["messages"].append(f"You : {user_input}")
+
+        # Get chatbot Response 
+        responses = stream_graph_updates(user_input)
+
+        for response in responses:
+            st.session_state["messages"].append(f"Assistant : {response}")
+
+# Main Page for displaying chat history
+def display_chat():
+    st.write("### Conversation")
+    for message in st.session_state['messages']:
+        st.write(message)
+
+# Run the sidebar and main chat display
+chatbot_sidebar() # input and submit  button on the sidebar
+display_chat() # Display the conversation on the mainpage
+
+
---
+++ secret_api_keys.py
@@ -0,0 +1,3 @@
+Groq_API_Key = "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"
+Tavily_api_key  = "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"
+riza_api_key = "riza_01JFCT1SAZ0AY1X37784KW0F7Y_01JFCT2TV597ADQJW4HNK0TCP9"
